a0aa3c325ffb3cc910dff4ed36da7dd589df6afe
Airat <airat2yakupov@yandex.ru>
2025-04-27 19:55:23 +0300

    feat: добавлены репозитории и crud для чатов

--- app/models/test_model.py
+++ app/models/test_model.py
@@ -1,8 +1,9 @@
 # src/auth/models.py
 import uuid
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
 from app.models.base_model import Base
-
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 class User(Base):
     __tablename__ = "users"
@@ -11,3 +12,39 @@ class User(Base):
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+
+
+class Group(Base):
+    __tablename__ = "groups"
+
+    id = Column(Integer, primary_key=True, index=True)
+    address = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=False)
+    members = relationship("GroupMember", back_populates="group")
+    messages = relationship("Message", back_populates="group")
+
+
+class GroupMember(Base):
+    __tablename__ = "group_members"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    group_id = Column(Integer, ForeignKey("groups.id"), nullable=False)
+
+    user = relationship("User", back_populates="groups")
+    group = relationship("Group", back_populates="members")
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    sender_id = Column(Integer, ForeignKey("users.id"))
+    sender_name = Column(String)
+    group_id = Column(Integer, ForeignKey("groups.id"))
+
+    sender = relationship("User", back_populates="messages")
+    group = relationship("Group", back_populates="messages")
+    unread_for = relationship("UnreadMessage", back_populates="message")
